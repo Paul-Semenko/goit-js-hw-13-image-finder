6b4d53aa6e39ff60934e18cbd9bfca364c5db53f
Paul-Semenko <78956330+Paul-Semenko@users.noreply.github.com>
2021-09-03 12:03:54 +0300

    commit-13-04

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,32 +1,33 @@
 const API_KEY = '23035596-1a90b7391e585725696c71550';
-const BASE_URL= 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api';
 
-export default class PhotoApiService{
-    constructor(){
-        this.searchQuery='';
-        this.page =1;
+export default class PhotoApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
     }
 
-    fetchPhoto(){
-        const url=`${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${this.searchQuery}&per_page=12&page=${this.page}`;
-    return fetch(url)
-        .then(response => response.json())
-        .then(({hits}) =>{this.page += 1;
-        return hits;
-        
-        })
-        .catch(error => console.log(error))
-    }
-    
-    resetPage(){
-        this.page =1;
+    async fetchPhoto() {
+        const url = `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${this.searchQuery}&per_page=12&page=${this.page}`;
+        try {
+            const response = await fetch(url);
+            const { hits } = await response.json();
+            this.page += 1;
+            return hits;
+        } catch (error) {
+            return console.log(error);
+        }
     }
 
-    get query(){
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
         return this.searchQuery;
     }
-    
-    set query(newQuery){
+
+    set query(newQuery) {
         this.searchQuery = newQuery;
     }
 
